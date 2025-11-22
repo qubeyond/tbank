25cb2acac8e9a6a258531d8f425a614a73153462
WhereIsSolar <jarformess@gmail.com>
2025-11-22 23:48:07 +0700

    New pages and edited pages.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import User from './pages/User'
 import AdmLogin from './pages/AdmLogin'
-import Admin from './pages/Admin' // ДОБАВЬТЕ ЭТОТ ИМПОРТ
+import Admin from './pages/Admin'
+import AdminQueue from './pages/AdminQueue'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/admlogin" element={<AdmLogin />} />
-        <Route path="/admin" element={<Admin />} /> {/* ДОБАВЬТЕ ЭТОТ МАРШРУТ */}
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/queues/:eventId" element={<AdminQueue />} />
         <Route path="/user" element={<User />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<AdmLogin />} />
